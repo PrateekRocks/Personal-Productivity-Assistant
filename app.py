54bd8e15a9a1d8b4d039cef719c38f8b5a92bdd5
PrateekRocks <prateek.magdumrocks@gmail.com>
2025-04-22 01:43:55 +0530

    check

--- app.py
+++ app.py
@@ -14,16 +14,16 @@ CORS(app)
 def home():
     return render_template("index.html")
 
-@app.route("/query",methods = ["POST"])
-def query_assistant():
-    data  = request.json
-    user_query = data.get("query" , "")
-
-    if not user_query:
-        return jsonify({"error" : "Query cannot be empty"}), 400
-
-    response = get_response(user_query)
-    return jsonify({"response" : response})
+# @app.route("/query",methods = ["POST"])
+# def query_assistant():
+#     data  = request.json
+#     user_query = data.get("query" , "")
+#
+#     if not user_query:
+#         return jsonify({"error" : "Query cannot be empty"}), 400
+#
+#     response = get_response(user_query)
+#     return jsonify({"response" : response})
 
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=8080 ,debug=True)
